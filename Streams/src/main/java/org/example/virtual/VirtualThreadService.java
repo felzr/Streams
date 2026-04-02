f7eea40c9d05485322ed215d6c94fb2c3489230f
felzr <felzr@outlook.com>
2026-04-02 13:41:52 -0300

    Add files via upload
    firts commit this project

---
+++ Streams/src/main/java/org/example/virtual/VirtualThreadService.java
@@ -0,0 +1,74 @@
+package org.example.virtual;
+
+import org.example.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class VirtualThreadService {
+
+    public long executeTasks(int numberOfTasks) throws InterruptedException {
+        ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
+
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i < numberOfTasks; i++) {
+            int taskId = i;
+            executor.submit(() -> simulateIO(taskId));
+        }
+
+        executor.shutdown();
+        executor.awaitTermination(5, TimeUnit.MINUTES);
+
+        long end = System.currentTimeMillis();
+
+        return end - start;
+    }
+
+    private void simulateIO(int taskId) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public long executeDatabaseSimulation(int numberOfTasks) throws InterruptedException {
+        ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
+
+        long start = System.currentTimeMillis();
+
+        List<Future<User>> futures = new ArrayList<>();
+
+        for (int i = 0; i < numberOfTasks; i++) {
+            int userId = i;
+
+            futures.add(executor.submit(() -> simulateDatabaseCall(userId)));
+        }
+
+        for (Future<User> future : futures) {
+            try {
+                future.get();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        executor.shutdown();
+        executor.awaitTermination(5, TimeUnit.MINUTES);
+
+        long end = System.currentTimeMillis();
+        return end - start;
+    }
+
+    private User simulateDatabaseCall(int userId) {
+        try {
+            Thread.sleep(50 + (int) (Math.random() * 100));
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        return new User(userId, "User_" + userId);
+    }
+}
